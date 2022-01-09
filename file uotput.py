4ed8fb39595d8b251880969e444eaca62e55cc4d
Shourya Mishra <96761798+bt21104082@users.noreply.github.com>
2022-01-09 19:05:36 +0530

    Add files via upload

---
+++ file uotput.py
@@ -0,0 +1,40 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: G:/My Drive/Classroom/coding/assignment 1.py ============
+ASSIGNMENT 1
+PYHTON PROGRAM TO FIND AVERAGE OF THREE GIVEN NUMBERS
+Enter the FIRST number:- 89
+Enter the SECOND number:- 78
+Enter the THIRD number:- 76
+The Average of three given numbers is:  81.0
+***
+PYTHON PROGRAM TO CALCULATE THE INCOME TAX OF PERSON AFTER SEVERAL DEDUCTION OF MONEY (GROSS INCOME>$30000)
+Please Enter the Gross Income:$ 45000
+Please Enter the number of dependents: 2
+The total income tax of a person in $:  34800.0
+***
+PYTHON PROGRAM TO STORE VALUES IN LISTS
+Enter the SID of the student= 21104082
+Enter the NAME of the student= Shourya Mishra
+Enter the GENDER M(MALE),F(FEMALE),U(UNKNOWN) use capital letters = M
+Enter the COURSE NAME of the student= Introduction to Computing
+Enter the CGPA of the student= 9.80
+[21104082, 'Shourya Mishra', 'M', 'Introduction to Computing', 9.8]
+***
+PYTHON PROGRAM TO ENTER THE MARKS OF FIVE STUDENTS
+Enter the Marks of First Student:89
+Enter the Marks of Second Student: 76
+Enter the Marks of Third Student: 97
+Enter the Marks of Fourth Student: 54
+Enter the Marks of Fifth Student: 23
+The Lists of marks of five students are as follows:  [89, 76, 97, 54, 23]
+***
+PYTHON PROGRAM TO PRINT A SPECIFIED LIST IN A MANNER
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
+***
+Instructed by- Manish Sir
+Student Name- Shourya Mishra
+SID-21104082
+Branch- Electrical
