5f206204b614b4cbb0863d123f6ac0af0dd9ba08
Shourya Mishra <96761798+bt21104082@users.noreply.github.com>
2022-01-26 16:11:46 +0530

    Add files via upload

---
+++ assignment2 output.py
@@ -0,0 +1,43 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: G:/My Drive/Classroom/coding/assignment 2 code.py ==========
+(a)
+Give the statement:Python is a case sensitive language.
+Python is a case sensitive language.
+36
+(b)
+.egaugnal evitisnes esac a si nohtyP
+(c)
+a case sensitive
+(d)
+Python is object oriented language
+(e)
+10
+(f)
+Pythonisacasesensitivelanguage.
+Hey,Shourya Mishra Here!
+My SID is 21104082
+I am from EE department and my CGPA is 9.9
+(a)
+0b1000
+(b)
+0b111010
+(c)
+0b110010
+(d)
+0b11100000
+0b101000
+(e)
+0b1110
+0b0
+Give a number:6
+Give a number:4
+Give a number:5
+6 is greatest number
+Give a word:Shourya
+No
+Give a number:8
+Give a number:5
+Give a number:2
+The triangle is not possible
