7d66910317d5fbd96ddd3b516c68d78dde47fe1f
Shourya Mishra <96761798+bt21104082@users.noreply.github.com>
2022-02-14 22:49:33 +0530

    Add files via upload

---
+++ pyhton assignment output.py
@@ -0,0 +1,82 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:/Users/STARNET/Downloads/python assignment code.py ========
+Answer 1.
+Enter a string: hello
+{'h': 1, 'e': 1, 'l': 2, 'o': 1}
+ 
+ 
+ 
+
+
+Answer 2.
+Day- 12
+Month- 08
+Year- 2002
+13 / 8 / 2002
+ 
+ 
+ 
+
+
+Answer 3.
+Enter a list of numbers: 6
+[(6, 36)]
+ 
+ 
+ 
+
+
+Answer 4.
+Enter Grade Point: 9
+Your Grade is 'A' and Excellent
+
+
+Answer 5.
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+ 
+ 
+ 
+
+
+Answer 6.
+Enter SID: 21104082
+Enter Name: Shourya Mishra
+Do you want to enter another student entry(Y or N):N
+a. {21104082: 'Shourya Mishra'}
+b. {21104082: 'Shourya Mishra'}
+c. {21104082: 'Shourya Mishra'}
+Search Name with SID: 21104082
+d. Shourya Mishra
+ 
+ 
+ 
+
+
+Answer 7.
+How long should be the sequence? 56
+[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141, 267914296, 433494437, 701408733, 1134903170, 1836311903, 2971215073, 4807526976, 7778742049, 12586269025, 20365011074, 32951280099, 53316291173, 86267571272, 139583862445, 225851433717]
+The average of the series is  10558691604.964285
+ 
+ 
+ 
+
+
+Answer 8.
+a. {1, 3, 5, 6, 8}
+b. {3, 6, 8, 9, 13, 17}
+c. {1, 2, 4, 5}
+d. {6, 7, 8, 9, 10}
+e. {10, 7}
+ 
+ 
+ 
+Instructed by- Manish Sir
+Submitted by-Shourya Mishra
+SID-21104082
